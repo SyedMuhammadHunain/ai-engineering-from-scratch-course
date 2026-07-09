9b072e9272944810ace354eac362a0f0e3585936
SyedMuhammadHunain <hunainsince2005@gmail.com>
2026-07-09 20:19:48 +0500

    Implement and compare Google GenAI SDK with raw HTTP request

--- api-and-keys-file.py
+++ api-and-keys-file.py
@@ -1,39 +1,44 @@
-# from google import genai
+from google import genai
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
-# client = genai.Client();
+# Google SDK Request
+client_sdk = genai.Client();
 
-# response = client.interactions.create(
-#     model='gemini-flash-lite-latest',
-#     input='Write a short poem about the beauty of nature.',
-# );
+response_of_sdk = client_sdk.interactions.create(
+    model='gemini-flash-lite-latest',
+    input='Write a short poem about the beauty of nature.',
+);
 
-# print(response.output_text);
-
-import requests
-
-url="https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent"
-
-response = requests.post(
-    url,
-    headers={
-        "Content-Type": "application/json",
-        "X-goog-api-key": os.getenv(),
-    },
-    json={
+# Raw HTTP Request
+url="https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-lite-latest:generateContent"
+headers = {
+    "Content-Type": "application/json",
+    "X-goog-api-key": os.getenv("GEMINI_API_KEY"),
+}
+json_data = {
         "contents": [
             {
                 "parts": [
                     {
-                        "text": "Explain how AI works in a few words"
+                        "text": "Write a short poem about the beauty of nature."
                     }
                 ]
             }
         ]
-    },
+    }
+response_of_rawhttp = requests.post(
+    url,
+    headers=headers,
+    json=json_data
 );
 
-print(response.json());
+# Response Comparison
+print(response_of_sdk);
+print("\n\n\n");
+print(response_of_rawhttp.json());
+
+
