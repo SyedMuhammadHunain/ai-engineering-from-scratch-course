b4ed3f8970e96ab4a42d65124700d4de6171924d
SyedMuhammadHunain <hunainsince2005@gmail.com>
2026-07-09 16:59:16 +0500

    Initial commit

---
+++ api-and-keys-file.py
@@ -0,0 +1,14 @@
+# from google import genai
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+
+# client = genai.Client();
+
+# response = client.interactions.create(
+#     model='gemini-flash-lite-latest',
+#     input='Write a short poem about the beauty of nature.',
+# );
+
+# print(response.output_text);
